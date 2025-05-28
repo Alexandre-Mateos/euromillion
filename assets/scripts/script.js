fdcd97a1f6d995cba9e3463f2cbe46ba88f10c75
alexandre-mateos <mateosalexandre95@gmail.com>
2025-05-28 15:43:58 +0200

    ajout d'une condition pour retirer les valeurs lorsqu'elles sont déjà présente dans le tableau de résultat

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,10 +2,37 @@ const buttonMainGrid = document.querySelectorAll(".container-main-grid button");
 // console.log(buttonMainGrid);
 const selectedNumber = [];
 
-for ( let i = 0 ; i < buttonMainGrid.length ; i++){
+// for ( let i = 0 ; i < buttonMainGrid.length ; i++){
+//     buttonMainGrid[i].addEventListener('click', ()=>{
+//         // console.log(`le bouton ${i+1} vient d'être cliqué`);
+//         selectedNumber.push(buttonMainGrid[i].innerHTML);
+//         // console.log(selectedNumber);
+//     } )
+// };
+
+/*
+stock dans le tableau selectedNumber la valeur du bouton cliqué
+verifie si la valeur existe déjà dans le tableau selectedNUmber
+si elle n'y est pas => l'ajoute
+si elle y est => la retire
+*/
+function selectNumber (){
+    for ( let i = 0 ; i < buttonMainGrid.length ; i++){
     buttonMainGrid[i].addEventListener('click', ()=>{
-        // console.log(`le bouton ${i+1} vient d'être cliqué`);
-        selectedNumber.push(buttonMainGrid[i].innerHTML);
+        let buttonValue = buttonMainGrid[i].innerHTML;
+        if (selectedNumber.includes(buttonValue)){
+            const index = selectedNumber.indexOf(buttonValue);
+            selectedNumber.splice(index, 1);
+        }else{
+        selectedNumber.push(buttonValue);
+        }
         console.log(selectedNumber);
     } )
 };
+}
+
+function isSelected (){
+    
+}
+
+selectNumber();
