f4d6741c6e8ef3f07e1c79362732c58fabc07ebd
alexandre-mateos <mateosalexandre95@gmail.com>
2025-05-28 16:29:22 +0200

    petite remise au propre

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,14 +2,6 @@ const buttonMainGrid = document.querySelectorAll(".container-main-grid button");
 // console.log(buttonMainGrid);
 const selectedNumber = [];
 
-// for ( let i = 0 ; i < buttonMainGrid.length ; i++){
-//     buttonMainGrid[i].addEventListener('click', ()=>{
-//         // console.log(`le bouton ${i+1} vient d'être cliqué`);
-//         selectedNumber.push(buttonMainGrid[i].innerHTML);
-//         // console.log(selectedNumber);
-//     } )
-// };
-
 /*
 Listener qui parcours la collection de bouton
 au click appelle la fonction arrayNumber() et applique le style approprié
