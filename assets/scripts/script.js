121c380b68a6aeaf54b832975be612c4ec17a253
alexandre-mateos <mateosalexandre95@gmail.com>
2025-05-28 16:21:31 +0200

    ajout de commentaires

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,27 +11,32 @@ const selectedNumber = [];
 // };
 
 /*
-stock dans le tableau selectedNumber la valeur du bouton cliqué
+Listener qui parcours la collection de bouton
+au click appelle la fonction arrayNumber() et applique le style approprié
+*/
+function selectNumber (){
+    for ( let i = 0 ; i < buttonMainGrid.length ; i++){
+        buttonMainGrid[i].addEventListener('click', ()=>{
+            let buttonValue = buttonMainGrid[i].innerHTML;   
+            arrayNumber(buttonValue);
+            buttonMainGrid[i].classList.toggle("selectedStyle");
+            console.log(selectedNumber);
+        } )
+    }
+}
+
+/* 
 verifie si la valeur existe déjà dans le tableau selectedNUmber
 si elle n'y est pas => l'ajoute
 si elle y est => la retire
 */
-function selectNumber (){
-    for ( let i = 0 ; i < buttonMainGrid.length ; i++){
-    buttonMainGrid[i].addEventListener('click', ()=>{
-        let buttonValue = buttonMainGrid[i].innerHTML;
-        if (selectedNumber.includes(buttonValue)){
-            const index = selectedNumber.indexOf(buttonValue);
-            selectedNumber.splice(index, 1);
-        }else{
+function arrayNumber (buttonValue){
+    if (selectedNumber.includes(buttonValue)){
+        const index = selectedNumber.indexOf(buttonValue);
+        selectedNumber.splice(index, 1);
+    }else if (selectedNumber.length <= 5){
         selectedNumber.push(buttonValue);
-        }
-        buttonMainGrid[i].classList.toggle("selectedStyle");
-        console.log(selectedNumber);
-    } )
     }
 }
-
-
 
 selectNumber();
